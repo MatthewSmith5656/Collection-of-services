daed27f98e5b12c0cbd5954077371431ddd44cd9
Matthew Smith <Matthew.Smith@ncp.co.uk>
2022-04-22 11:07:12 +0100

    added model naming convention

--- FunctionApps/Application/Models/QueueMessage.cs
+++ FunctionApps/Application/Models/QueueMessage.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace FunctionApps.Application.Models
 {
     public class QueueMessage : Entity
     {
+        [JsonPropertyName("message")]
         public string Message { get; set; }
+
+        [JsonPropertyName("keyValuePairs")]
         public IDictionary<int, string> KeyValuePairs { get; set; }
     }
 }
